ffb2194aa0ecdc1259a86dce1795307db7b79c1d
benjamin-shih <66041726+benjamin-shih@users.noreply.github.com>
2020-06-02 11:44:55 -0700

    Update df_generation.py
    feed_gen is kind of redundant it just updates the data frame generated from df_gen so it only contains the data to be pulled in data_utils_PyTorch.py and nothing more

--- df_generation.py
+++ df_generation.py
@@ -8,15 +8,22 @@ Necessary fields:
     CLIENT_ID: Spotify client ID key
     CLIENT_SECRET: Spotify client secret key
     PLAYLIST_ID: Spotify ID for the playlist from which a dataframe is to be returned
+    CSV_PATH: Path to .csv file with play counts
 
-Returns a Pandas DataFrame of the form:
-  Column1  |   Column2   | Column3 |  Column4  |  Column5 |    Column6
---------------------------------------------------------------------------
-  Song ID  | duration_ms |   key   | loudness  |   tempo  | time_signature
+df_gen returns a Pandas DataFrame of the form:
+  Column1 | Column2 | Column3 |    Column4   |  Column5 |  Column6 | Column7 |   Column8   |    Column9
+----------|---------|---------|--------------|----------|----------|---------|-------------|---------------
+   artist |  song   |   id    |  play count  |   key    | loudness |  tempo  | duration_ms | time_signature
+
+feed_gen returns a Pandas DataFrame of the form: (this is the DataFrame we feed into the net)
+   Column1   | Column2 |  Column3  |  Column4 |  Column5    |    Column6
+-------------|---------|-----------|----------|-------------|---------------
+  play count |   key   | loudness  |   tempo  | duration_ms | time_signature
 '''
 CLIENT_ID = '0ae72c9b0d4948d5ba0206c4c350d3fe'
 CLIENT_SECRET = 'c1db0c06c2df46b7b9219250080adc8d'
 PLAYLIST_ID = '4Kvvbir7qtwhF5xIeRILOl'
+CSV_PATH = 'drive/My Drive/predictify/data_table_2.csv'
 
 #Authorization
 token = sp.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
@@ -56,11 +63,34 @@ def get_playlist_features(track_ids):
 
   return usable_track_features
 
+def get_artist_song_ids(csv_path=CSV_PATH):
+  with open(csv_path, newline='') as csvfile:
+      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+      #row_count = sum([1 for row in spamreader])
+      all_rows_df = pd.DataFrame(columns = ['artist', 'song', 'id', 'play count'])
+      for row in spamreader:
+          url = urllib.parse.unquote(row[1] + "%20artist:" + row[0])
+          search_result = spotify.search(url, type='track', limit=1)
+          try:
+            curr_row_df = pd.DataFrame({'artist': [search_result.get('tracks')['items'][0]['artists'][0].get('name')], #artist name
+                                        'song': [search_result.get('tracks')['items'][0]['name']], #song name
+                                        'id': [search_result.get('tracks')['items'][0]['id']], #song id
+                                        'play count': row[2]})
+            all_rows_df = all_rows_df.append(curr_row_df)
+          except:
+            continue
+      return all_rows_df
 #Create a dataframe with the specified features for all song IDs in the playlist passed in
 def df_gen(playlist_id=PLAYLIST_ID):
     ids = get_playlist_ids(playlist_id)
     feat = get_playlist_features(ids)
-    id_df = pd.DataFrame.from_dict(ids, orient = "columns")
     feat_df = pd.DataFrame.from_dict(feat, orient = "columns")
-    df = id_df.join(feat_df)
+    #add play count to features dataframe
+    df = pd.merge(get_artist_song_ids(), feat_df, on='id')
+    df.drop(0, axis=1)
     return df
+#Generate feed dataframe whcih contains explicit x_train and y_train data
+def feed_gen(playlist_id=PLAYLIST_ID):
+    tot = df_gen(playlist_id)
+    feed = tot.drop([0,1,2])
+    return feed
